9cc9fec82afb2b853d79c43583081b0e378b495c
Aurel <aurelticot.dev@gmail.com>
2020-10-24 13:17:23 +1100

    handle allowed domains configuration

--- src/@types/node/index.d.ts
+++ src/@types/node/index.d.ts
@@ -2,8 +2,9 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     PORT?: string;
     WEB_CONCURRENCY?: string;
-    NODE_ENV?: string;
+    NODE_ENV?: "production" | "development" | "test";
     ENVIRONMENT_ID?: string;
     LOG_LEVEL?: string;
+    ALLOWED_DOMAIN?: string;
   }
 }
--- src/config/index.ts
+++ src/config/index.ts
@@ -5,9 +5,13 @@ const port: number = process.env.PORT ? parseInt(process.env.PORT) : 9000;
 const concurrency: number = process.env.WEB_CONCURRENCY
   ? parseInt(process.env.WEB_CONCURRENCY)
   : numCPUs;
-const nodeEnv: string = process.env.NODE_ENV || "development";
+const nodeEnv: "production" | "development" | "test" =
+  process.env.NODE_ENV || "development";
 const environmentId: string = process.env.ENVIRONMENT_ID || "unknown";
 const logLevel: string | undefined = process.env.LOG_LEVEL;
+const allowedDomains: RegExp[] = process.env.ALLOWED_DOMAIN
+  ? process.env.ALLOWED_DOMAIN.split(",").map((domain) => new RegExp(domain))
+  : [];
 
 export const config = {
   concurrency,
@@ -15,4 +19,5 @@ export const config = {
   nodeEnv,
   environmentId,
   logLevel,
+  allowedDomains,
 };
